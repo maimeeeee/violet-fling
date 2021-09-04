e8493131be37a3e68e16d21e9ada21ec4e35b270
mai_hirose <wind.close.cloud.girl@gmail.com>
2021-09-04 14:45:04 +0900

    feat:getFiletree

--- server/api/filetree/controller.ts
+++ server/api/filetree/controller.ts
@@ -1,7 +1,13 @@
 import { defineController } from './$relay'
-import { createFileTree } from '$/service/filetree'
+import { createFileTree, getFiles } from '$/service/filetree'
 
-export default defineController(() => ({
+const print = (text: string) => console.log(text)
+export default defineController({ getFiles, print }, ({ getFiles, print }) => ({
+  get: async ({ query }) => {
+    if (query?.message) print(query.message)
+
+    return { status: 200, body: await getFiles(query?.limit) }
+  },
   post: async ({ body }) => ({
     status: 201,
     body: await createFileTree(body.filename, body.parentid)
--- server/service/filetree.ts
+++ server/service/filetree.ts
@@ -4,8 +4,13 @@ import type { FileTree, Prisma } from '$prisma/client'
 
 const prisma = new PrismaClient()
 
-export const getFilenamebyId = (fileid: FileTree['id']) =>
-  prisma.fileTree.findMany({ where: { id: fileid } })
+export const getFiles = depend(
+  { prisma: prisma as { filetree: { findMany(): Promise<FileTree[]> } } },
+  async ({ prisma }, limit?: number) => (await prisma.filetree.findMany()).slice(0, limit)
+)
 
 export const createFileTree = (filename: FileTree['filename'], parentid: FileTree['parentid']) =>
   prisma.fileTree.create({ data: { filename: filename, parentid: parentid } })
+
+export const deleteFile = (fileid: FileTree['id']) =>
+  prisma.fileTree.delete({ where: { id: fileid } })
